a045873bde544a84bd28711c3f2eff3b7a53582b
Samson <isamu3636136@gmail.com>
2019-06-03 15:04:40 -0500

    finish basic

--- Basic/password.py
+++ Basic/password.py
@@ -5,8 +5,8 @@ PASSWORD = 'python'
 answer = input('give me the right password: ')
 attempt = 1
 while answer != PASSWORD:
-  attempt += 1
-  answer = input('this is not the right password, try it again: ')
-  if attempt > 3:
-    sys.exit('you have missed it too many times')
+    attempt += 1
+    answer = input('this is not the right password, try it again: ')
+    if attempt > 3:
+        sys.exit('you have missed it too many times')
 print('welcome!')
--- Basic/ticket.py
+++ Basic/ticket.py
@@ -1,20 +1,31 @@
 Ticket_price = 100
+Servise_charge = 2
 
 tickets_remaining = 14
 
-# output how many tickets remaining using the tr vurable
 
-print('there are {} tickets remaining now'.format(tickets_remaining))
+def calc_price(x):
+    return (x * Ticket_price) + Servise_charge
 
-# gather the user's name and assign it to new vurable
+while tickets_remaining > 1:
+    print('there are {} tickets remaining now'.format(tickets_remaining))
+    User_name = raw_input('What is your name? ')
+    total_tickets = input('Hi {}! How many tickets do you want to buy? '.format(User_name))
+    try:
+        total_tickets = int(total_tickets)
+        if total_tickets > tickets_remaining:
+            raise ValueError('sorry, there is no more than {} tickets'.format(tickets_remaining))
+    except ValueError as err:
+        print('that is nat a valid value {} please run it again'.format(err))
+    else:
+        # total_price = total_tickets * Ticket_price
+        total_price = calc_price(total_tickets)
+        print('total is {}'.format(total_price))
+        proc = raw_input('Do you wanna proceed? Y/N ')
+        if proc == 'Y':
+            print('sold')
+            tickets_remaining -= total_tickets
+        else:
+            print('thank you {}!'.format(User_name))
 
-User_name = raw_input('What is your name? ')
-
-# ask how many tickets the user want
-
-total_tickets = raw_input('Hi {}! How many tickets do you want to buy? '.format(User_name))
-total_tickets = int(total_tickets)
-
-total_price = total_tickets * Ticket_price
-
-print('total is {}'.format(total_price))
+print('Sorry the tickets is sold')
